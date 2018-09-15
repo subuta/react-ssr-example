bfb1a28dac210ccaa6ed0afc029caed71dcca78e
subuta <shunta.3110@gmail.com>
2018-09-15 18:49:28 +0900

    Fix page.

--- src/lib/page.js
+++ src/lib/page.js
@@ -1,58 +1,16 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import loadable from 'loadable-components' // eslint-disable-line
 import _ from 'lodash'
 
-import pMinDelay from 'p-min-delay'
-
-import log from 'lib/utils/log'
-import {
-  getInitialProps,
-  getInitialPropsFromContext
-} from 'lib/utils/initialProps'
-import { isBrowser } from 'lib/utils/env'
-import asPage from 'lib/hocs/asPage'
-
 import Page404 from 'lib/components/404'
-import unwrapModule from 'lib/utils/unwrapModule'
 
-const delay = promise => { // eslint-disable-line
-  if (!isBrowser) return promise
-  // Pass-through for initial render.
-  if (getInitialProps()) return promise
-  // Minimum delay for loading.
-  return pMinDelay(promise, 200)
-}
-
-const renderLoadable = ({ Component, loading, error, ownProps }) => { // eslint-disable-line
-  if (loading) return <div>Loading...</div>
-  if (error) return <div>Oops! {error.message}</div>
-
-  // Get react-router's staticContext.
-  const context = _.get(ownProps, 'staticContext', {})
-  let initialProps = getInitialPropsFromContext(context)
-
-  return <Component {...initialProps} {...ownProps} />
-}
-
-const onImportError = (err) => { // eslint-disable-line
-  if (err.message.match(/Cannot find module '.*\.js'/)) {
-    log('[404]', err)
-    return Page404
-  }
-
-  console.error('Unknown error', err)
-  // Render null component otherwise.
-  return () => null
-}
-
-const createLoadable = (module) => async () => { // eslint-disable-line
-  const Component = unwrapModule(await delay(module).catch(onImportError))
-  return asPage(Component)
-}
+/* eslint-disable */
+import loadable from 'loadable-components'
+import createLoadable, {
+  renderLoadable
+} from 'lib/utils/createLoadable'
 
 // Pages will be injected to here for better hmr support.
-/* eslint-disable */
 export const Pages = []
 /* eslint-enable */
 
--- src/lib/utils/createLoadable.js
+++ src/lib/utils/createLoadable.js
@@ -1,28 +1,14 @@
 import React from 'react'
 import _ from 'lodash'
 
-import pMinDelay from 'p-min-delay'
-
 import {
-  getInitialProps,
   getInitialPropsFromContext
 } from 'lib/utils/initialProps'
-import { isBrowser } from 'lib/utils/env'
 import asPage from 'lib/hocs/asPage'
 
 import unwrapModule from 'lib/utils/unwrapModule'
 
-// Wait at least 200ms before show component.
-const delay = promise => { // eslint-disable-line
-  if (!isBrowser) return promise
-  // Pass-through for initial render.
-  if (getInitialProps()) return promise
-  // Minimum delay for loading.
-  return pMinDelay(promise, 200)
-}
-
-// Render loadable component with Loading/Error state.
-export const renderLoadable = ({ Component, loading, error, ownProps }) => { // eslint-disable-line
+export const renderLoadable = ({ Component, loading, error, ownProps }) => {
   if (loading) return <div>Loading...</div>
   if (error) return <div>Oops! {error.message}</div>
 
@@ -33,10 +19,8 @@ export const renderLoadable = ({ Component, loading, error, ownProps }) => { // 
   return <Component {...initialProps} {...ownProps} />
 }
 
-// Create react-loadable component with HOC.
-const createLoadable = (module) => async () => { // eslint-disable-line
-  const Component = unwrapModule(await delay(module))
-  return asPage(Component)
+export const createLoadable = (module) => {
+  return asPage(unwrapModule(module))
 }
 
 export default createLoadable
